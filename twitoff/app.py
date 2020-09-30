f1384a78a6b5d0df39a0c9800cfa5b320faa948e
zacksnyder-lsds <zacksnyder1998@gmail.com>
2020-09-30 00:17:53 -0400

    lecture one add-on

--- twitoff/app.py
+++ twitoff/app.py
@@ -15,7 +15,13 @@ def create_app():
     def root():
         return 'Welcome to Twitoff!'
 
+    @app.route('/<username>/<followers>')
+    def add_user(username, followers):
+        user = User(username=username, followers=followers)
+        DB.sessions.add(user)
+        DB.sessions.commit()
 
+        return f'{username} has been added to the DB'
 
     return app
     #from twitoff.db_model import DB, User, Tweet
